786c8ecc1a8ff23d2dd033e4666c2644d2e87b60
DhruvVasisht <vasisht.dhruv7@gmail.com>
2023-07-30 12:38:43 +0530

    Lecture 1

---
+++ src/Variables.java
@@ -0,0 +1,19 @@
+public class Variables {
+    public static void main(String[] args) {
+        /*
+        /int age=26;
+        int count=12;
+        System.out.println(age);
+        System.out.print(count);
+        */
+
+        /*float pi= 3.14f;
+        double secondPi= 3.14;
+        System.out.println(pi);
+        System.out.println(secondPi);
+        int age=150;
+        byte newAge=(byte)age;
+        System.out.println(newAge);*/
+
+    }
+}
