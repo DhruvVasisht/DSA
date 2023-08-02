28e2fbb854f5abf79da7b600e666d0cad3df4847
DhruvVasisht <vasisht.dhruv7@gmail.com>
2023-08-02 12:51:11 +0530

    Lecture 4

--- src/Lecture4/loops.java
+++ src/Lecture4/loops.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class loops {
     public static void main(String[] args) {
         //for(int i=0;i<=5;i++) {
-        //System.out.println("Hello");
+        System.out.println("Hello");
         //System.out.println(i);
 
         Scanner sc = new Scanner(System.in);
